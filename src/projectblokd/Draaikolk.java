2177166f575f0d942ac7a9a245f9243d2bdbb1cc
timedelaar <timedelaar@ziggo.nl>
2015-06-15 12:51:51 +0200

    - draaikolk verdwijnt tijdelijk door kogel

--- src/projectblokd/Draaikolk.java
+++ src/projectblokd/Draaikolk.java
@@ -4,18 +4,48 @@
  */
 package projectblokd;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import javax.swing.Timer;
+
 /**
  *
  * @author Tim
  */
 public class Draaikolk extends Water {
     
+    private boolean kanVerplaatsen = false;
+    Timer kolkTimer;
+    
     public Draaikolk () {
         setImage("draaikolk.png");
+        kolkTimer = new Timer(1500, new ActionListener(){
+
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                kanVerplaatsen = false;
+                setImage("draaikolk.png");
+                kolkTimer.stop();
+                getVeld().repaint();
+            }
+        });
     }
     
     @Override
     public boolean kanVerplaatsen (Held held) {
-        return false;
+        return kanVerplaatsen;
+    }
+    
+    @Override
+    public boolean kanVerplaatsen (Kogel kogel) {
+        return kanVerplaatsen;
+    }
+    
+    @Override
+    public void destroy (Iterator<SpelItem> iter) {
+        kanVerplaatsen = true;
+        setImage("water.png");
+        kolkTimer.start();
     }
 }
